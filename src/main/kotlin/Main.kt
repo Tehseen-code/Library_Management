44a5b43ab02c5ab077f108f20d38f6dba867e3ee
Tehseen ul hassan <tehseen161nb@gmail.com>
2025-08-28 12:46:25 +0500

    Added Member class with encapsulation + demo (Task 1.2)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,38 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.example.classes.Book
+import org.example.classes.Magazine
+import org.example.classes.Member
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main(){
+    val book1 = Book("B001", "Kotlin for Beginners", "Tehssen ul hassan ", "978-1234567890", 250)
+    val book2 = Book("B002","Advanced Kotlin", "Hassan ", "978-1234567", 300)
+    val mag1 = Magazine("M001", "Tech Monthly", 45, "TechPress")
+
+    val member = Member("M001", "Alice Johnson", "alice@email.com")
+
+    println("Member Info: ${member.displayInfo()}")
+
+    println("Borrowing Items...")
+
+    // Borrow book1 and mag1
+    member.borrowItem(book1)
+    member.borrowItem(mag1)
+
+
+    println("Member Info After Borrowing: ${member.displayInfo()}")
+
+    // Try borrowing book1 again (should fail, already borrowed)
+    member.borrowItem(book1)
+
+    // Return magazine
+    member.returnItem(mag1)
+
+    println("Member Info After Returning: ${member.displayInfo()}")
+
+    // Calculate late fees for 5 days
+    val lateFees = member.calculateTotalLateFees(5)
+    println("Total Late Fees for 5 days: $$lateFees")
+
+
 }
---
+++ src/main/kotlin/classes/Member.kt
@@ -0,0 +1,70 @@
+package org.example.classes
+
+class Member(
+    private val memberId: String,
+    private var name: String,
+    private var email: String
+) {
+    //A list of items currently borrowed by member
+
+    private val borrowedItems=mutableListOf<LibraryItem>()
+
+    fun getMemberId(): String=memberId
+
+    fun getName():String=name
+
+    fun setName(newName: String) {
+        if (newName.isNotBlank()) {
+            name = newName
+        } else {
+            println("Name cannot be empty.")
+        }
+    }
+
+    fun getEmail():String=email
+
+    fun setEmail(newEmail: String) {
+        if(newEmail.contains("@")){
+            email=newEmail
+        }
+        else{
+            println("Email cannot be empty or wrong format.")
+        }
+    }
+
+    //Add library item to borrowed item to library list if item is available
+
+    fun borrowItem(item: LibraryItem){
+        if(item.isAvailable){
+            borrowedItems.add(item)
+            item.isAvailable = false // false after that item to borrow because no one can borrow before return
+            println("${item.title} borrowed successfully by $name")
+        }else{
+            println("${item.title} is not available for borrowing.")
+        }
+    }
+
+    fun returnItem(item: LibraryItem){
+        if(borrowedItems.contains(item)){
+            borrowedItems.remove(item)
+            item.isAvailable=true
+            println("${item.title} returned successfully by $name")
+        }else{
+            println("${item.title} was not borrowed by $name.")
+        }
+    }
+
+    fun calculateTotalLateFees(daysLate: Int): Double{
+        var totalFee=0.0
+
+        for (item in borrowedItems){
+            totalFee+=item.calculateFee(daysLate)
+        }
+        return totalFee
+    }
+
+
+    fun displayInfo(): String{
+        return "MemberID: $memberId, Name: $name, Email: $email, Borrowed: ${borrowedItems.size} items"
+    }
+}
